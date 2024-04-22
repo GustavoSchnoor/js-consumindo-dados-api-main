818942ad3ffe2f00c5ddb7c68a338f6f5b95beba
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-22 11:34:21 -0300

    Terminei curso

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.querySelector('#erro');
+    mensagemErro.innerHTML = '';
+
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         consultaCEPConvertida = await consultaCEP.json();
@@ -7,10 +10,23 @@ async function buscaEndereco(cep) {
             throw Error('CEP não existente!')
         }
 
+        var cidade = document.querySelector('#cidade');
+        var logradouro = document.querySelector('#endereco');
+        var estado = document.querySelector('#estado');
+
+        // AQUI NESSES 3, estamos acessando o OBJETO ja convertido json da API(consultaCEPConvertida), com os dados e acessando o valor das chaves correspondentes
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro; 
+        estado.value = consultaCEPConvertida.uf;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
 
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP informado esta incorreto!</p>`;
         console.log(erro);
     }
 }
+
+var cep = document.querySelector('#cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value)) // Colocamos a nossa função criada dentro de outra função, pois se colocarmos a função criada direto com o cep.value, ele vai simplesmente executar assim que chegar nessa linha, e nao queremos isso, queremos que ele execute, SOMENTE quando sair do foco do campo (focusout)
